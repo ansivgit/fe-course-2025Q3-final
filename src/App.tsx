ccbf8d3f048ccd5f2f2b969a628c4a894dcf5ad4
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-02-11 21:37:25 +0400

    chore: add classnames

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import type { ReactElement } from 'react';
+import styles from './App.module.css';
 
 export function App(): ReactElement {
   return (
     <div>
-      <h1>Hello, Tandem</h1>
+      <h1 className={styles.title}>Hello, Tandem</h1>
     </div>
   );
 }
---
+++ src/custom.d.ts
@@ -0,0 +1,5 @@
+/* biome-ignore lint/style/noDefaultExport: classnames */
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
