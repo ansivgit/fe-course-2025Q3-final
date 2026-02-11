8d58074d98943f490f5b444787a99f26b9da2613
ansivgit <ansiv@yandex.ru>
2026-02-11 21:59:38 +0300

    fix: biome issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import classNames from 'classnames/bind';
 import type { ReactElement } from 'react';
 import styles from './App.module.css';
+
+const cx = classNames.bind(styles);
 
 export function App(): ReactElement {
   return (
     <div>
-      <h1 className={styles.title}>Hello, Tandem</h1>
+      <h1 className={cx('title')}>Hello, Tandem</h1>
     </div>
   );
 }
