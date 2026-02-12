bb022d061c7ac60c612f4f276572b202fe83e949
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-02-12 18:29:12 +0400

    refactor: remove redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import styles from '@/App.module.css';
 
 import classNames from 'classnames/bind';
 import type { ReactElement } from 'react';
-import { mockUsers } from './../data/user';
 
 const cx = classNames.bind(styles);
 
 export function App(): ReactElement {
   return (
     <div>
-      <h2 className={cx('title')}>Users</h2>
-      <ul>
-        {mockUsers.map((user) => (
-          <li key={user.id}>
-            <h3>{user.name}</h3>
-          </li>
-        ))}
-      </ul>
+      <h1 className={cx('title')}>Hello, Tandem</h1>
     </div>
   );
 }
