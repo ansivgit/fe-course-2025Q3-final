f197b0b027a2cf80ca8b77adb1b54890de96b09b
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-13 13:12:56 +0300

    feat: add classNames bind

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,10 @@
-import classNames from 'classnames';
-import styles from './button.module.css';
+import classNames from 'classnames/bind';
+import styles from '@/components/button/button.module.css';
 
 import type { ButtonProps } from '@/types/index';
 import type { ReactElement } from 'react';
+
+const cx = classNames.bind(styles);
 
 export const Button = ({
   children,
@@ -14,12 +16,12 @@ export const Button = ({
 }: ButtonProps): ReactElement => {
   return (
     <button
-      className={classNames({
-        [styles.button]: true,
-        [styles[`button--color--${color}`]]: color,
-        [styles[`button--size--${size}`]]: size,
-        [className ?? '']: Boolean(className),
-      })}
+      className={cx(
+        'button',
+        `button--color--${color}`,
+        `button--size--${size}`,
+        className,
+      )}
       disabled={disabled}
       {...rest}
     >
@@ -27,3 +29,4 @@ export const Button = ({
     </button>
   );
 };
+
