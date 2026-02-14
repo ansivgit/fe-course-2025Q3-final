34aca41a94d946da4c4bff1500d5903b6e0d26c6
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-15 02:48:47 +0300

    feat: add subtitle component

---
+++ src/components/subtitle/subtitle.tsx
@@ -0,0 +1,36 @@
+import styles from '@/components/subtitle/subtitle.module.css';
+
+import classNames from 'classnames/bind';
+import type { ElementType, ReactElement, ReactNode } from 'react';
+
+const cx = classNames.bind(styles);
+
+export const SUBTITLE_LEVEL_2 = 2;
+export const SUBTITLE_LEVEL_3 = 3;
+
+type SubtitleLevel = typeof SUBTITLE_LEVEL_2 | typeof SUBTITLE_LEVEL_3;
+type SubtitleSize = 'medium' | 'large';
+type SubtitleColor = 'white' | 'red';
+type SubtitleMargin = 'compact' | 'short' | 'default' | 'spacious' | 'extraspacious';
+
+type SubtitleProps = {
+  children: ReactNode;
+  className?: string;
+  level?: SubtitleLevel;
+  size?: SubtitleSize;
+  color?: SubtitleColor;
+  margin?: SubtitleMargin;
+};
+
+export const Subtitle = ({
+  children,
+  className,
+  level = SUBTITLE_LEVEL_3,
+  size = 'medium',
+  color = 'white',
+  margin = 'default',
+}: SubtitleProps): ReactElement => {
+  const Tag: ElementType = `h${level}`;
+
+  return <Tag className={cx('subtitle', size, color, margin, className)}>{children}</Tag>;
+};
