d2776f2cd254460945b426d7c488cafec66dc7f6
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-16 00:07:00 +0300

    feat: add layout component

---
+++ src/components/layout/layout.tsx
@@ -0,0 +1,14 @@
+import styles from '@/components/layout/layout.module.css';
+
+import classNames from 'classnames/bind';
+import type { ReactElement, ReactNode } from 'react';
+
+const cx = classNames.bind(styles);
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps): ReactElement => {
+  return <div className={cx('layout')}>{children}</div>;
+};
