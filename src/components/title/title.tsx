f5ecfb5620bee5db0ecd18250e3346974e778b64
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-15 21:04:05 +0300

    fix: edit according to comments

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -6,23 +6,13 @@ import type { ReactElement, ReactNode } from 'react';
 const cx = classNames.bind(styles);
 
 type TitleSize = 'small' | 'medium' | 'large';
-type TitleMargin = 'default' | 'compact';
-type TitleWeight = 'semibold' | 'bold';
 
 type TitleProps = {
   children: ReactNode;
   className?: string;
   size?: TitleSize;
-  margin?: TitleMargin;
-  weight?: TitleWeight;
 };
 
-export const Title = ({
-  children,
-  className,
-  size = 'medium',
-  margin = 'default',
-  weight = 'bold',
-}: TitleProps): ReactElement => {
-  return <h1 className={cx('title', size, margin, weight, className)}>{children}</h1>;
+export const Title = ({ children, className, size = 'medium' }: TitleProps): ReactElement => {
+  return <h1 className={cx('title', size, className)}>{children}</h1>;
 };
