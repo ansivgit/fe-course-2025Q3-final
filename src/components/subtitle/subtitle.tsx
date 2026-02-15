c637c5aa72349c6521f47d73b79eb9d7771adb93
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-15 22:30:26 +0300

    refactor: delete unused props

--- src/components/subtitle/subtitle.tsx
+++ src/components/subtitle/subtitle.tsx
@@ -9,25 +9,19 @@ export const SUBTITLE_LEVEL_2 = 2;
 export const SUBTITLE_LEVEL_3 = 3;
 
 type SubtitleLevel = typeof SUBTITLE_LEVEL_2 | typeof SUBTITLE_LEVEL_3;
-type SubtitleSize = 'medium' | 'large';
-type SubtitleColor = 'white' | 'red';
 
 type SubtitleProps = {
   children: ReactNode;
   className?: string;
   level?: SubtitleLevel;
-  size?: SubtitleSize;
-  color?: SubtitleColor;
 };
 
 export const Subtitle = ({
   children,
   className,
   level = SUBTITLE_LEVEL_3,
-  size = 'medium',
-  color = 'white',
 }: SubtitleProps): ReactElement => {
   const Tag: ElementType = `h${level}`;
 
-  return <Tag className={cx('subtitle', size, color, className)}>{children}</Tag>;
+  return <Tag className={cx('subtitle', className)}>{children}</Tag>;
 };
