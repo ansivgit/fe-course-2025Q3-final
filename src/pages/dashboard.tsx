9642ff6462950bc48ef2b1b63c1392316d729741
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-17 00:57:28 +0300

    feat: add router navigation

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,16 @@
+import { Button } from '@/components/button/button';
 import { Title } from '@/components/title/title';
 
 import type { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Dashboard = (): ReactElement => {
   return (
     <div>
       <Title>Dashboard</Title>
+      <Link to="/practice">
+        <Button>Go to Practice</Button>
+      </Link>
     </div>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,20 @@
+import { Button } from '@/components/button/button';
 import { Title } from '@/components/title/title';
 
 import type { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Login = (): ReactElement => {
+  const navigate = useNavigate();
+
+  const handleLogin = (): void => {
+    void navigate('/dashboard');
+  };
+
   return (
     <div>
       <Title>Login</Title>
+      <Button onClick={handleLogin}>Login</Button>
     </div>
   );
 };
