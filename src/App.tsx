a3c3da3be6511cc833be61c8f1967b5861229783
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-17 23:05:03 +0300

    refactor: 33 - move app router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import '@/styles/index.css';
 
-import { AppRouter } from '@/router';
+import { Landing } from '@/pages/landing';
+import { Login } from '@/pages/login';
+import { NotFound } from '@/pages/not-found';
+import { Practice } from '@/pages/practice';
+import { ROUTES } from '@/constants/constants';
 
 import type { ReactElement } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 export function App(): ReactElement {
   return <AppRouter />;
 }
+
+export const AppRouter = (): ReactElement => (
+  <Routes>
+    <Route path={ROUTES.landing} element={<Landing />} />
+    <Route path={ROUTES.login} element={<Login />} />
+    <Route path={ROUTES.practice} element={<Practice />} />
+    <Route path={ROUTES.notFound} element={<NotFound />} />
+  </Routes>
+);
