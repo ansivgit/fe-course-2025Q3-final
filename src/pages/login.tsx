cea83b7f428bd1b14c1685ec3c7638f87659b317
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2026-02-21 01:25:01 +0400

    feat: 41 - improve page styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,18 @@
-import { Layout } from '@/components/layout/layout';
 import { LoginForm } from '@/components/login-form/login-form';
+import { Logo } from '@/components/logo/logo';
+import { Title } from '@/components/title/title';
 
 import type { ReactElement } from 'react';
 
 export const Login = (): ReactElement => {
   return (
-    <Layout verticalAlign="center">
+    <div className="center">
+      <div className={'title-section'}>
+        <Logo />
+        <Title>Welcome to the platform!</Title>
+        <p className={'description'}>Join us and start learning</p>
+      </div>
       <LoginForm />
-    </Layout>
+    </div>
   );
 };
