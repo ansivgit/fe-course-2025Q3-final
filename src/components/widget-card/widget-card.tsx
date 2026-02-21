806d02f42fb195a4ec445acf57eae8aee2fe9a0c
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-21 23:12:46 +0300

    fix: 24 - add icon class

--- src/components/widget-card/widget-card.tsx
+++ src/components/widget-card/widget-card.tsx
@@ -64,7 +64,7 @@ export const WidgetCard = ({
         </span>
 
         <span className={cx('meta-item')}>
-          <div className={cx('meta-image')}>
+          <div className={cx('meta-action-image')}>
             <ClockIcon />
           </div>
           {time}
