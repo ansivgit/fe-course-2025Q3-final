ededfb74922685421630c8905272cc6f5e2d5413
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-21 17:48:24 +0300

    feat: 24 - add svgr plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 
 /* biome-ignore lint/style/noDefaultExport: Vite requires default export */
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': new URL('src', import.meta.url).pathname,
