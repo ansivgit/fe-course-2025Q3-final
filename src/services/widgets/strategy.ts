e76f5f62b3d8dfc69ca8a8e2c72e07b0b07fae49
Erika Milevskaya <rika.gumerova@gmail.com>
2026-02-22 22:08:40 +0300

    fix: 45 - change strategy validate logic

--- src/services/widgets/strategy.ts
+++ src/services/widgets/strategy.ts
@@ -1,6 +1,6 @@
-import type { QuizAnswer, QuizWidget, WidgetStrategy } from '@/types/widgets';
+import type { QuizValidationResult, QuizWidgetStrategy } from '@/types/widgets';
 
-export const quizStrategy: WidgetStrategy<QuizWidget, QuizAnswer> = {
+export const quizStrategy: QuizWidgetStrategy = {
   type: 'quiz',
 
   run: (widget, _onAnswer) => {
@@ -24,17 +24,27 @@ export const quizStrategy: WidgetStrategy<QuizWidget, QuizAnswer> = {
 
   validate: (widget, answer) => {
     const correctAnswersIds = widget.payload.correctAnswersIds;
-    const selected = answer.selectedIds;
+    const selectedIds = answer.selectedIds;
 
-    const isCorrect =
-      selected.length === correctAnswersIds.length &&
-      selected.every((id) => correctAnswersIds.includes(id));
+    const allAnswersIds = new Set([...selectedIds, ...correctAnswersIds]);
+    const result: QuizValidationResult = {};
 
-    console.log('Selected:', answer.selectedIds);
-    console.log(isCorrect ? '✅ Correct!' : '❌ Wrong!');
+    allAnswersIds.forEach((id) => {
+      if (selectedIds.includes(id)) {
+        result[id] = correctAnswersIds.includes(id) ? 'correct' : 'wrong';
+      } else {
+        result[id] = 'missed';
+      }
+    });
+
+    const isCorrect = Object.values(result).every((state) => state === 'correct');
+
+    console.log('Selected:', selectedIds);
+    console.log('Validation result:', result);
     console.log('Explanation:', widget.payload.explanation);
+    console.log('Boolean result:', isCorrect ? '✅ Correct' : '❌ Wrong');
     console.log('---');
 
-    return isCorrect;
+    return { isCorrect, result };
   },
 };
--- src/types/widgets.ts
+++ src/types/widgets.ts
@@ -29,12 +29,27 @@ export type QuizAnswer = {
   selectedIds: string[];
 };
 
+export type WidgetAnswerMap = {
+  quiz: QuizAnswer;
+};
+
 export type WidgetStrategy<T extends Widget, A> = {
   type: T['type'];
   run(widget: T, onAnswer: (answer: A) => void): void;
   validate(widget: T, answer: A): boolean;
 };
 
-export type WidgetAnswerMap = {
-  quiz: QuizAnswer;
+export type QuizWidgetStrategy = {
+  type: 'quiz';
+  run(widget: QuizWidget, onAnswer: (answer: QuizAnswer) => void): void;
+  validate(widget: QuizWidget, answer: QuizAnswer): QuizValidateReturn;
+};
+
+type OptionValidationState = 'correct' | 'wrong' | 'missed';
+
+export type QuizValidationResult = Record<string, OptionValidationState>;
+
+export type QuizValidateReturn = {
+  isCorrect: boolean;
+  result: QuizValidationResult;
 };
