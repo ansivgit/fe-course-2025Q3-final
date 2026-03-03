90eb7385b850ea45a12059f97399b9e94b7b2756
ansivgit <ansiv@yandex.ru>
2026-03-03 14:42:00 +0300

    Revert "Merge pull request #73 from ansivgit/dev"
    This reverts commit c22f4aa02e1e9804a93785c30cdd6216462a29eb, reversing
    changes made to 882e4a9e205a76e5399f82b0c2e167bc81f6223e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
 
 /* biome-ignore lint/style/noDefaultExport: Vite requires default export */
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': new URL('src', import.meta.url).pathname,
     },
   },
-  base: '/',
 });
