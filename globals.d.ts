298d841d8847fa4080309b6df0ccab3a06a7544c
ansivgit <ansiv@yandex.ru>
2026-03-03 14:40:45 +0300

    Revert "Merge pull request #73 from ansivgit/dev"
    This reverts commit c22f4aa02e1e9804a93785c30cdd6216462a29eb, reversing
    changes made to 882e4a9e205a76e5399f82b0c2e167bc81f6223e.

--- globals.d.ts
+++ globals.d.ts
@@ -2,12 +2,3 @@ declare module '*.module.css' {
   const classes: Record<string, string>;
   export default classes;
 }
-
-declare module '*.svg?react' {
-  import * as React from 'react';
-  const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default ReactComponent;
-}
-
