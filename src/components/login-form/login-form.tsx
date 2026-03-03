137f118e851494b7bdf4a0cf2d466e643c69ba5a
ansivgit <ansiv@yandex.ru>
2026-03-03 14:53:25 +0300

    Reapply 'Merge pull request #73 from ansivgit/dev' This reverts commit 298d84

---
+++ src/components/login-form/login-form.tsx
@@ -0,0 +1,95 @@
+import classNames from 'classnames/bind';
+import { type SyntheticEvent, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Button } from '@/components/button/button';
+import { login, register } from '@/services/api/auth';
+import { ROUTES } from '@/constants/constants';
+
+import type { AuthFormFields } from '@/types/user';
+
+import { LoginInput } from './inputs/login-input';
+import { PasswordInput } from './inputs/password-input';
+import { UserNameInput } from './inputs/user-name-input';
+import styles from './login-form.module.css';
+
+const cx = classNames.bind(styles);
+
+export const LoginForm = ({ page = 'login' }: { page?: string }) => {
+  const navigate = useNavigate();
+
+  const [loginValue, setLoginValue] = useState('');
+  const [userNameValue, setUserNameValue] = useState('Anonymous');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [isFormValid, setFormValid] = useState(false);
+  const [errors, setErrors] = useState({ login: false, userName: false, password: false });
+  const [formErrorMessage, setFormErrorMessage] = useState('');
+
+  const isRegisterPage = page === 'register';
+
+  const checkFormValidity = (): boolean => {
+    if (formErrorMessage) {
+      return false;
+    }
+
+    return Object.values(errors).every((error) => !error) && !!loginValue && !!passwordValue;
+  };
+
+  useEffect(() => {
+    checkFormValidity();
+  }, [loginValue, passwordValue, formErrorMessage]);
+
+  const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault();
+
+    if (isFormValid) {
+      const result = isRegisterPage
+        ? await register({ login: loginValue, name: userNameValue, password: passwordValue })
+        : await login({ login: loginValue, password: passwordValue });
+
+      if (result.error) {
+        setFormErrorMessage(result.error.message);
+        setFormValid(false);
+        void navigate(`/${ROUTES.practice}`); //! o nly for testing! remove this line!
+      } else if (result.data) {
+        void navigate(`/${ROUTES.practice}`);
+      } else {
+        console.error('Unexpected login result:', result);
+      }
+    }
+  };
+
+  const createFieldHandler = (
+    fieldName: keyof AuthFormFields,
+    setValue: (value: string) => void,
+  ) => {
+    return (value: string, isValid: boolean): void => {
+      setFormErrorMessage('');
+
+      setValue(value);
+      setErrors((previous) => ({
+        ...previous,
+        [fieldName]: !isValid,
+      }));
+
+      setFormValid(checkFormValidity());
+    };
+  };
+
+  return (
+    <div>
+      <form className={cx('form')} onSubmit={handleSubmit}>
+        <LoginInput onInputChange={createFieldHandler('login', setLoginValue)} />
+        {isRegisterPage && (
+          <UserNameInput onInputChange={createFieldHandler('name', setUserNameValue)} />
+        )}
+        <PasswordInput onInputChange={createFieldHandler('password', setPasswordValue)} />
+
+        <Button type="submit" size="large" disabled={!isFormValid}>
+          Login
+        </Button>
+
+        <p className={cx('error')}>{formErrorMessage}</p>
+      </form>
+    </div>
+  );
+};
---
+++ src/components/widgets/quiz-widget/quiz-widget.tsx
@@ -0,0 +1,88 @@
+import classNames from 'classnames/bind';
+import { type ReactElement, useState } from 'react';
+import { Button } from '@/components/button/button';
+import { Paragraph } from '@/components/paragraph/paragraph';
+import { Subtitle } from '@/components/subtitle/subtitle';
+import { AnswerOption } from '@/components/widgets/answer-option/answer-option';
+import { getOptionStatus } from '@/components/widgets/helpers';
+import { ALPHABET } from '@/constants/constants';
+
+import type { QuizPayload, WidgetProps } from '@/types/widgets';
+
+import styles from './quiz-widget.module.css';
+
+const cx = classNames.bind(styles);
+
+export function Quiz({ widget, onAnswer, onNext }: WidgetProps) {
+  const [selectedIds, setSelectedIds] = useState<string[]>([]);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const changeStatus = (id: string): void => {
+    if (isSubmitted) {
+      return;
+    }
+    setSelectedIds((selected) =>
+      selected.includes(id) ? selected.filter((item) => item !== id) : [...selected, id],
+    );
+  };
+
+  const handleSubmit = (): void => {
+    if (selectedIds.length === 0) {
+      return;
+    }
+    setIsSubmitted(true);
+  };
+
+  const handleNext = (): void => {
+    onAnswer({ selectedIds });
+    if (onNext) {
+      onNext();
+    }
+  };
+
+  return (
+    <>
+      <div className={cx('quiz-widget')}>
+        <Subtitle level="h2">{widget.payload.question}</Subtitle>
+        <ul className={cx('options')}>
+          {widget.payload.options.map((option, index) => {
+            const status = getOptionStatus(option, selectedIds, isSubmitted, widget);
+            const label = ALPHABET[index];
+            return (
+              <AnswerOption
+                key={option.id}
+                option={option}
+                label={label}
+                status={status}
+                onOptionClick={() => {
+                  changeStatus(option.name);
+                }}
+              />
+            );
+          })}
+        </ul>
+        {isSubmitted && Explanation(widget.payload.explanation)}
+      </div>
+      <div className={cx('button-container')}>
+        <Button
+          disabled={selectedIds.length === 0 && !isSubmitted}
+          onClick={isSubmitted ? handleNext : handleSubmit}
+        >
+          {isSubmitted ? 'Next' : 'Submit'}
+        </Button>
+      </div>
+    </>
+  );
+}
+
+function Explanation(explanation: QuizPayload['explanation']): ReactElement | undefined {
+  if (!explanation) {
+    return;
+  }
+  return (
+    <div className={cx('explanation')}>
+      <h4>Explanation</h4>
+      <Paragraph text={explanation} />
+    </div>
+  );
+}
