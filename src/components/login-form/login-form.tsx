23d2fabf00c7de2ec471156fb976ff585082b7b6
Erika Milevskaya <rika.gumerova@gmail.com>
2026-03-10 21:01:01 +0300

    fix: 95 - edit user store

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -3,7 +3,7 @@ import { type SyntheticEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { Button } from '@/components/button/button';
 import { login, register } from '@/services/api/auth';
-import { userStore } from '@/store/store';
+import { useUserStore } from '@/store/store';
 import { ROUTES } from '@/constants/constants';
 
 import type { AuthFormFields } from '@/types/user';
@@ -25,7 +25,7 @@ export const LoginForm = ({ page = 'login' }: { page?: string }) => {
   const [errors, setErrors] = useState({ login: false, userName: false, password: false });
   const [formErrorMessage, setFormErrorMessage] = useState('');
 
-  const { setUser } = userStore();
+  const { setUser } = useUserStore();
 
   const isRegisterPage = page === 'register';
 
@@ -53,11 +53,9 @@ export const LoginForm = ({ page = 'login' }: { page?: string }) => {
         setFormErrorMessage(result.error.message);
         setFormValid(false);
         void navigate(`/${ROUTES.practice}`); //! o nly for testing! remove this line!
-        console.log(userStore.getState());
       } else if (result.data) {
         setUser(result.data);
         void navigate(`/${ROUTES.practice}`);
-        console.log(userStore.getState());
       } else {
         console.error('Unexpected login result:', result);
       }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,22 @@
 import { create } from 'zustand';
-import type { Session, User } from '@//types/user';
+
+import type { Session, User } from '@/types/user';
 
 export type UserStore = {
   user: User | null;
-  isLoggedIn: boolean;
-  loading: boolean;
-  error: string | null;
 
   setUser: (user: User | null) => void;
   addSession: (session: Session) => void;
-  setLoading: (loading: boolean) => void;
-  setError: (error: string | null) => void;
 };
 
-export const userStore = create<UserStore>((set, get) => ({
+export const useUserStore = create<UserStore>((set, get) => ({
   user: null,
   isLoggedIn: false,
   loading: false,
   error: null,
 
   setUser: (user): void => {
-    set({ user, isLoggedIn: !!user });
+    set({ user });
   },
 
   addSession: (session): void => {
@@ -33,13 +29,5 @@ export const userStore = create<UserStore>((set, get) => ({
         },
       });
     }
-  },
-
-  setLoading: (loading): void => {
-    set({ loading });
-  },
-
-  setError: (error): void => {
-    set({ error });
   },
 }));
