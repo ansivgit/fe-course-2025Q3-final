326517432575abf2f4ba2ba3c2bc9ac814c80a99
Erika Milevskaya <rika.gumerova@gmail.com>
2026-03-14 00:12:20 +0300

    refactor: 92 - remove format data constant

--- src/utils/format-data.ts
+++ src/utils/format-data.ts
@@ -1,4 +1,4 @@
-import { MS_IN_SECOND } from '@/constants/constants';
+const MS_IN_SECOND = 1000;
 
 export const formatTimestampToMonthYear = (timestamp: string | number): string => {
   return new Date(Number(timestamp) * MS_IN_SECOND).toLocaleDateString('en-US', {
